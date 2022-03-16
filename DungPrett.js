1d70997730000403f6a3e7f698386f56d7df930d
harun5897 <harungb025@gmail.com>
2022-03-16 14:00:08 +0700

    4 commit

--- DungPrett.js
+++ DungPrett.js
@@ -1,11 +1,14 @@
 const DungPrett = (num) => {
+    var res = '';
     for(let i = 1; i <= num; i++) {
-        if(i % 3 == 0 && i % 5 == 0) console.log('DungPrett')
-        else if(i % 3 == 0) console.log('Dung') 
-        else if(i % 5 == 0) console.log('Prett') 
-        else console.log(i)
+        if(i % 3 == 0 && i % 5 == 0) res += 'DungPrett'
+        else if(i % 3 == 0) res += 'Dung'
+        else if(i % 5 == 0) res += 'Prett'
+        else res += String(i);
+        res += ' ';
 
     }
+    console.log(res)
 }
 
 const num = 20;
